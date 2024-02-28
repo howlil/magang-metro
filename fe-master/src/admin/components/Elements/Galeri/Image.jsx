a149e7c1f92087953e73251699f5592482027453
syhana <syakina.tech@gmail.com>
2024-02-28 20:47:47 +0700

    add komponen galeri

---
+++ fe-master/src/admin/components/Elements/Galeri/Image.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import s from "./image.module.css";
+import silang from "/public/silang.svg";
+import add from "/public/addimg.svg";
+
+const Image = () => {
+  const [images, setImages] = useState([]);
+
+  const handleImageChange = (e) => {
+    const files = e.target.files;
+    const newImages = Array.from(files).map(file => URL.createObjectURL(file));
+    setImages(prevImages => [...prevImages, ...newImages]);
+  };
+
+  const handleRemoveImage = (index) => {
+    const updatedImages = [...images];
+    updatedImages.splice(index, 1);
+    setImages(updatedImages);
+  };
+
+  const handleAddImage = () => {
+    document.getElementById('fileInput').click();
+  };
+
+  return (
+    <div className={s.flex}>
+      <input
+        id="fileInput"
+        type="file"
+        accept="image/*"
+        multiple
+        style={{ display: 'none' }}
+        onChange={handleImageChange}
+      />
+      <button onClick={handleAddImage} className={s.button}>
+        <div>
+          <img src={add} alt='addimage'/>
+        </div>
+        <h1>Klik untuk <br /> tambah foto</h1>
+      </button>
+      {images.map((image, index) => (
+          <div key={index} className={s.container}>
+            <button className={s.close} onClick={() => handleRemoveImage(index)}>
+              <img width="7rem" height="7rem" src={silang} alt='silang'/>
+            </button>
+            <img src={image} className={s.layout} alt={`Image ${index + 1}`} />      
+          </div>
+      ))}
+        
+    </div>
+  );
+};
+
+export default Image;
--- fe-master/src/admin/pages/galeri/index.jsx
+++ fe-master/src/admin/pages/galeri/index.jsx
@@ -1,11 +1,19 @@
 import AdminLayout from "../../components/Layout/AdminLayout";
+import Image from "../../components/Elements/Galeri/Image";
+import Title from "../../components/Elements/Title/Title";
+import { Helmet } from "react-helmet";
 
 
 export default function Galeri() {
   return (
     <>
+    <Helmet>
+        <title>Galeri | Fiable Low Office</title>
+      </Helmet>
     <AdminLayout>
-      hdllo
+      <Title title="Kumpulan Foto"/>
+      <Image>
+      </Image>
     </AdminLayout>
     </>
   )
